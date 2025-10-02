0e417559d149393673b4691baca930cdefa226a4
ratikant-behera <beheraratikanta436@gmail.com>
2025-10-02 12:09:51 +0530

    Create findKthLargest.java

---
+++ findKthLargest.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num:nums){
+            maxHeap.add(num);
+        }
+        for(int i=0;i<k-1;i++){
+            maxHeap.poll();
+        }
+        return maxHeap.peek();
+    }
+}
